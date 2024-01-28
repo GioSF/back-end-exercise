0beb62cef5d388307646fa13b9e90b0a44f5d6dd
GioSF <giosfdev@gmail.com>
2024-01-28 18:44:20 -0300

    Updates Lesson model - implements code to get lessons watched achievements data for a given user

--- app/Models/Lesson.php
+++ app/Models/Lesson.php
@@ -2,8 +2,11 @@
 
 namespace App\Models;
 
+use App\Models\LessonUser;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Lesson extends Model
 {
@@ -15,6 +18,57 @@ class Lesson extends Model
      * @var array
      */
     protected $fillable = [
+        'id',
         'title'
     ];
+
+    public function users(): BelongsToMany
+	{
+        return $this->belongsToMany(User::class)->using(LessonUser::class);
+	}
+
+    static public function lessonsWatchedAchievementsMap(): array
+    {
+        return [
+            1 => 'First Lesson Watched',
+            5 => '5 Lessons Watched',
+            10 => '10 Lessons Watched',
+            25 => '25 Lessons Watched',
+            50 => '50 Lessons Watched',
+        ];
+    }
+
+    public function getUnlockedAchievement(User $user)
+    {
+        $userLessonsCount = $user->lessons()->where('watched', true)->count();
+        $achievementsList = self::lessonsWatchedAchievementsMap();
+        $hasNewAchievement = array_key_exists($userLessonsCount, $achievementsList);
+        $unlockedAchievementName = $hasNewAchievement ? $achievementsList[$userLessonsCount] : null;
+
+        return $unlockedAchievementName;
+    }
+
+    static public function getUserLessonsWatchedAchievementsData(User $user): array
+    {
+        $lessonsWatched = $user->lessons()
+            ->where('watched', true)
+            ->count();
+        $achivements = [];
+
+        foreach(self::lessonsWatchedAchievementsMap() as $requiredAmount => $achievementLabel)
+        {
+            if($lessonsWatched >= $requiredAmount)
+            {
+                $achivements['unlocked_achievements'][] = $achievementLabel;
+            }
+            else
+            {
+                $achivements['next_available_achievements'][] = $achievementLabel;
+                break;
+            };
+        }
+
+        return $achivements;
+    }
+
 }
