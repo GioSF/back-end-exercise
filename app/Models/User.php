3da32db75d6c80e069e12560b4e8729ff8cc75c3
GioSF <giosfdev@gmail.com>
2024-01-28 19:15:28 -0300

    Implements code to get achievements and badges data on User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,9 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\Comment;
+use App\Models\Lesson;
+use App\Models\LessonUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -43,6 +46,8 @@ class User extends Authenticatable
         'password' => 'hashed',
     ];
 
+    private $userAchievementsData;
+
     /**
      * The comments that belong to the user.
      */
@@ -56,7 +61,7 @@ class User extends Authenticatable
      */
     public function lessons()
     {
-        return $this->belongsToMany(Lesson::class);
+        return $this->belongsToMany(Lesson::class)->using(LessonUser::class);
     }
 
     /**
@@ -66,5 +71,70 @@ class User extends Authenticatable
     {
         return $this->belongsToMany(Lesson::class)->wherePivot('watched', true);
     }
-}
 
+    public function getUserEntitiesAchievementsData(): array
+    {
+        $commentAchievements = Comment::getUserCommentsWrittenAchievementsData($this);
+        $lessonAchievements = Lesson::getUserLessonsWatchedAchievementsData($this);
+
+        return array_merge_recursive(
+            $lessonAchievements,
+            $commentAchievements,
+        );
+    }
+
+    public function getUserBadgesMap(): array
+    {
+        return [
+            0 => 'Beginner',
+            4 => 'Intermediate',
+            8 => 'Advanced',
+            10 => 'Master',
+        ];
+    }
+
+    public function getUserAccomplishmentsData(): array
+    {
+        $userAchievementsCount = $this->getUserAchievementsCount();
+        $userBadgesData = [];
+
+        foreach ($this->getUserBadgesMap() as $requiredAmount => $badgeLabel)
+        {
+            if ($userAchievementsCount >= $requiredAmount)
+            {
+                $userBadgesData['current_badge'] = $badgeLabel;
+            }
+            else
+            {
+                $userBadgesData['next_badge'] = $badgeLabel;
+                $userBadgesData['remaining_to_unlock_next_badge'] = $requiredAmount - $userAchievementsCount;
+                break;
+            }
+        }
+
+        return array_merge_recursive(
+            $this->userAchievementsData,
+            $userBadgesData,
+        );
+    }
+
+    public function getUserAchievementsCount(): int
+    {
+        $this->userAchievementsData = $this->getUserEntitiesAchievementsData();
+        $isUnlockedAchievementsSet = isset($this->userAchievementsData['unlocked_achievements']);
+        $userAchievementsCount = $isUnlockedAchievementsSet ? count($this->userAchievementsData['unlocked_achievements']) : 0;
+
+        return $userAchievementsCount;
+    }
+
+    public function getUnlockedBadgeName()
+    {
+        $userAchievementsCount = $this->getUserAchievementsCount();
+        $badgesList = $this->getUserBadgesMap();
+        $hasNewBadge = array_key_exists($userAchievementsCount, $badgesList);
+        $unlockedBadgeName = $hasNewBadge ? $badgesList[$userAchievementsCount] : null;
+
+        return $unlockedBadgeName;
+    }
+
+}
