6bfa3656d50e6bb0d8fc09cada55e34678f196ca
GioSF <giosfdev@gmail.com>
2024-01-28 18:52:36 -0300

    Implements settings for tests on DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comment;
 use App\Models\Lesson;
+use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,7 +16,32 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $lessons = Lesson::factory()
+            ->count(200)
+            ->create();
+        $users = User::factory()
             ->count(20)
             ->create();
+        $comment = Comment::factory()
+            ->count(500)
+            ->create();
+        $counter = 0;
+
+        $lessons = Lesson::all();
+        $counter = 51;
+
+        $users = User::all();
+
+        foreach ($users as $user)
+        {
+            $lessons = Lesson::where('id', '<', $counter)->get();
+            foreach($lessons as $lesson)
+            {
+                // $isWatched = true;
+                $isWatched = (bool) rand(0,2);
+                $user->lessons()->attach($lesson->id, ['watched' => $isWatched]);
+            }
+            $counter = $counter - 7;
+        }
     }
 }
+
