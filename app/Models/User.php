5da35d9ef37f6c8006ca9dc52a36cc5a46554924
GioSF <giosfdev@gmail.com>
2024-01-29 18:50:43 -0300

    Removes reference to pivot table used during implementation

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,7 +5,6 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use App\Models\Comment;
 use App\Models\Lesson;
-use App\Models\LessonUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
